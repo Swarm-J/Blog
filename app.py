a575e0f51d4fdd3c62b0f7b8ee6fc194c2685e55
Swarm-J <julianschinkel@gmail.com>
2022-05-01 16:41:14 +0200

    updated blog project

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key yo"
+
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # filters #
 
@@ -38,6 +47,22 @@ def page_not_found(error):
 @app.errorhandler(500)
 def page_not_found(error):
     return render_template('500.html'), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data= ''
+
+    return render_template('name.html', 
+        name=name, 
+        form=form)
+
+
+
 
 
 
