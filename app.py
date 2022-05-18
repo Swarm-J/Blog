1619ab699a7b69f20c187b16a166f894fa52678d
Swarm-J <julianschinkel@gmail.com>
2022-05-18 13:19:49 +0200

    tweaked app for heroku

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "my super secret key yo"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:870113069FL4$K1987!@localhost/users'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qezklsctbeikek:fddd3677a94d2ebadf114ba51807f4640e361e751c7b675746afa865f6a94d49@ec2-34-201-95-176.compute-1.amazonaws.com:5432/d4gkapb8k6ks97'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mbugrcmssmfldg:d373f3fdc04f35936c6c54a1fc29c0720d10c5fca1f3e4444d1eb5d4eafd2bc4@ec2-34-201-95-176.compute-1.amazonaws.com:5432/d6qhttg89bl7v4'
 
 UPLOAD_FOLDER = 'static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
